54acbeb9aed835ac758fada00247351ed26e0667
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-09-25 22:12:28 -0400

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,26 +1,44 @@
-package Q17_18_Shortest_Supersequence;
+package Q10_11_Peaks_and_Valleys;
+
+import CtCILibrary.AssortedMethods;
 
 public class Tester {
-
-	/**
-	 * @param args
-	 */
+	public static boolean confirmValleyPeak(int[] array) {
+		for (int i = 1; i < array.length - 1; i++) {
+			int prev = array[i - 1];
+			int curr = array[i];
+			int next = array[i + 1];
+			if (prev <= curr && curr >= next) {
+				continue;
+			} else if (prev >= curr && curr <= next) {
+				continue;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
-		int[] array = {9, 5, 1, 0, 2, 1, 3, 5, 7, 9, 1, 1, 5, 8, 9, 7};
-		int[] set = {1, 5, 9};
-		
-		Range shortestA = QuestionA.shortestSupersequence(array, set);
-		Range shortestB = QuestionB.shortestSupersequence(array, set);
-		Range shortestC = QuestionC.shortestSupersequence(array, set);
-		Range shortestD = QuestionD.shortestSupersequence(array, set);
-		
-		
-		if (shortestA.length() != shortestB.length() || 
-			shortestB.length() != shortestC.length() || 
-			shortestC.length() != shortestD.length()) {
-			System.out.println("Mismatching.");
-		} else {
-			System.out.println("Matching: " + shortestA.length());
+		for (int i = 0; i < 100; i++) {
+			int[] originalArray = AssortedMethods.randomArray(10, 0, 100);
+			int[] arrayA = originalArray.clone();
+			int[] arrayB = originalArray.clone();
+			int[] arrayC = originalArray.clone();
+			//System.out.println(AssortedMethods.arrayToString(array));
+			QuestionA.sortValleyPeak(arrayA);
+			QuestionB.sortValleyPeak(arrayB);
+			QuestionC.sortValleyPeak(arrayC);
+			if (!confirmValleyPeak(arrayA) || !confirmValleyPeak(arrayB) || !confirmValleyPeak(arrayC)) {
+				System.out.println(AssortedMethods.arrayToString(originalArray));
+				System.out.println(AssortedMethods.arrayToString(arrayA));
+				System.out.println(AssortedMethods.arrayToString(arrayB));
+				System.out.println(AssortedMethods.arrayToString(arrayC));
+				System.out.println(confirmValleyPeak(arrayA));
+				System.out.println(confirmValleyPeak(arrayB));
+				System.out.println(confirmValleyPeak(arrayC));
+				break;
+			}
 		}
 	}
 
