7760e0fba05c556f12d9a3c9b1a27e2269abde9f
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-09-25 22:12:48 -0400

    Add files via upload

--- QuestionB.java
+++ QuestionB.java
@@ -1,46 +1,93 @@
-package Q10_11_Peaks_and_Valleys;
-
-import CtCILibrary.AssortedMethods;
+package Q10_05_Sparse_Search;
 
 public class QuestionB {
-	public static void swap(int[] array, int left, int right) {
-		int temp = array[left];
-		array[left] = array[right];
-		array[right] = temp;
-	}
-	
-	public static void sortValleyPeak(int[] array) {
-		for (int i = 1; i < array.length; i += 2) {
-			int biggestIndex = maxIndex(array, i - 1, i, i + 1);
-			if (i != biggestIndex) {
-				swap(array, i, biggestIndex);
+	public static int searchI(String[] strings, String str, int first, int last) {
+		while (first <= last) {
+			/* Move mid to the middle */
+			int mid = (last + first) / 2;
+			
+			/* If mid is empty, find closest non-empty string */
+			if (strings[mid].isEmpty()) { 
+				int left = mid - 1;
+				int right = mid + 1;
+				while (true) {
+					if (left < first && right > last) {
+						return -1;
+					} else if (right <= last && !strings[right].isEmpty()) {
+						mid = right;
+						break;
+					} else if (left >= first && !strings[left].isEmpty()) {
+						mid = left;
+						break;
+					}
+					right++;
+					left--;
+				}
+			}
+			
+			int res = strings[mid].compareTo(str);
+			if (res == 0) { // Found it!
+				return mid;
+			} else if (res < 0) { // Search right
+				first = mid + 1;
+			} else { // Search left
+				last = mid - 1;
 			}
 		}
+		return -1;
 	}	
 	
-	public static int maxIndex(int[] array, int a, int b, int c) {
-		int len = array.length;
-		int aValue = a >= 0 && a < len ? array[a] : Integer.MIN_VALUE; 
-		int bValue = b >= 0 && b < len ? array[b] : Integer.MIN_VALUE; 
-		int cValue = c >= 0 && c < len ? array[c] : Integer.MIN_VALUE; 
-		
-		int max = Math.max(aValue, Math.max(bValue, cValue));
-		
-		if (aValue == max) {
-			return a;
-		} else if (bValue == max) {
-		 	return b;
-		} else {
-			return c;
+	public static int searchR(String[] strings, String str, int first, int last) {
+		if (first > last) {
+			return -1;
 		}
+		
+		/* Move mid to the middle */
+		int mid = (last + first) / 2;
+		
+		/* If mid is empty, find closest non-empty string. */
+		if (strings[mid].isEmpty()) { 
+			int left = mid - 1;
+			int right = mid + 1;
+			while (true) {
+				if (left < first && right > last) {
+					return -1;
+				} else if (right <= last && !strings[right].isEmpty()) {
+					mid = right;
+					break;
+				} else if (left >= first && !strings[left].isEmpty()) {
+					mid = left;
+					break;
+				}
+				right++;
+				left--;
+			}
+		}
+			
+		/* Check for string, and recurse if necessary */
+		if (str.equals(strings[mid])) { // Found it!
+			return mid;
+		} else if (strings[mid].compareTo(str) < 0) { // Search right
+			return searchR(strings, str, mid + 1, last);
+		} else { // Search left
+			return searchR(strings, str, first, mid - 1);
+		}
+	}	
+		
+	public static int search(String[] strings, String str) {
+		if (strings == null || str == null || str.isEmpty()) {
+			return -1;
+		}
+		return searchR(strings, str, 0, strings.length - 1);
 	}
-
+	
 	public static void main(String[] args) {
-		int[] array = {48, 40, 31, 62, 28, 21, 64, 40, 23, 17};
-		System.out.println(AssortedMethods.arrayToString(array));
-		sortValleyPeak(array);
-		System.out.println(AssortedMethods.arrayToString(array));
-		System.out.println(Tester.confirmValleyPeak(array));
+        String[] stringList = {"apple", "", "", "banana", "", "", "", "carrot", "duck", "", "", "eel", "", "flower"};
+        System.out.println(search(stringList, "ac"));
+        
+		//for (String s : stringList) {
+		//	String cloned = new String(s);
+        //	System.out.println("<" + cloned + "> " + " appears at location " + search(stringList, cloned));
+		//}
 	}
-
 }
