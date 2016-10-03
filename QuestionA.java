7a2ed0afb210c2e12c6271af402f0fb92e71403d
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-10-03 18:49:25 -0400

    Add files via upload

--- QuestionA.java
+++ QuestionA.java
@@ -1,29 +1,42 @@
-package Q10_11_Peaks_and_Valleys;
-
-import java.util.Arrays;
-
-import CtCILibrary.AssortedMethods;
+package Q10_09_Sorted_Matrix_Search;
+import CtCILibrary.*;
 
 public class QuestionA {
-	public static void sortValleyPeak(int[] array) {
-		Arrays.sort(array);
-		for (int i = 1; i < array.length; i += 2) {
-			swap(array, i - 1, i);
-		}
+
+	public static boolean findElement(int[][] matrix, int elem) {
+		int row = 0;
+		int col = matrix[0].length - 1; 
+		while (row < matrix.length && col >= 0) {
+			if (matrix[row][col] == elem) {
+				return true;
+			} else if (matrix[row][col] > elem) {
+				col--;
+			} else {
+				row++; 
+			}
+		} 
+		return false; 
 	}
 	
-	public static void swap(int[] array, int left, int right) {
-		int temp = array[left];
-		array[left] = array[right];
-		array[right] = temp;
-	}
-
 	public static void main(String[] args) {
-		int[] array = {48, 40, 31, 62, 28, 21, 64, 40, 23, 17};
-		System.out.println(AssortedMethods.arrayToString(array));
-		sortValleyPeak(array);
-		System.out.println(AssortedMethods.arrayToString(array));
-		System.out.println(Tester.confirmValleyPeak(array));
+		int M = 10;
+		int N = 5;
+		int[][] matrix = new int[M][N];
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = 10 * i + j;
+			}
+		}
+		
+		AssortedMethods.printMatrix(matrix);
+		
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				int v = 10 * i + j;
+				System.out.println(v + ": " + findElement(matrix, v));
+			}
+		}
+		
 	}
 
 }
