201a0456821770efe5cb762203f9999746027b8a
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-10-03 18:49:50 -0400

    Add files via upload

--- Question.java
+++ Question.java
@@ -1,39 +1,60 @@
-package Q10_04_Sorted_Search_No_Size.Arrayish;
+package Q10_03_Search_in_Rotated_Array;
 
 public class Question {
+	
+	public static int search(int a[], int x) {
+		return search(a, 0, a.length - 1, x);
+	}
 
-	public static int binarySearch(Listy list, int value, int low, int high) {
-		int mid;
+	
+	public static int search(int a[], int left, int right, int x) {
+		int mid = (left + right) / 2;
+		if (x == a[mid]) { // Found element
+			return mid;
+		}
+		if (right < left) {
+			return -1;
+		}
 		
-		while (low <= high) {
-			mid = (low + high) / 2;
-			int middle = list.elementAt(mid);
-			if (middle > value || middle == -1) {
-				high = mid - 1;
-			} else if (middle < value) {
-				low = mid + 1;
+		/* While there may be an inflection point due to the rotation, either the left or 
+		 * right half must be normally ordered.  We can look at the normally ordered half
+		 * to make a determination as to which half we should search. 
+		 */
+		if (a[left] < a[mid]) { // Left is normally ordered.
+			if (x >= a[left] && x < a[mid]) { 
+				return search(a, left, mid - 1, x);
 			} else {
-				return mid;
+				return search(a, mid + 1, right, x);
+			}
+		} else if (a[mid] < a[left]) { // Right is normally ordered.
+			if (x > a[mid] && x <= a[right]) {
+				return search(a, mid + 1, right, x);
+			} else {
+				return search(a, left, mid - 1, x);
+			}				
+		} else if (a[left] == a[mid]) { // Left is either all repeats OR loops around (with the right half being all dups)
+			if (a[mid] != a[right]) { // If right half is different, search there
+				return search(a, mid + 1, right, x);
+			} else { // Else, we have to search both halves
+				int result = search(a, left, mid - 1, x); 
+				if (result == -1) {
+					return search(a, mid + 1, right, x); 
+				} else {
+					return result;
+				}
 			}
 		}
-		return -1;		
+		return -1;
 	}
-	
-	public static int search(Listy list, int value) {
-		int index = 1;
-		while (list.elementAt(index) != -1 && list.elementAt(index) < value) {
-			index *= 2;
-		}
-		return binarySearch(list, value, index / 2, index);
-	}
-	
+
 	public static void main(String[] args) {
-		int[] array = {1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18};
-		Listy list = new Listy(array);
-		for (int a : array) {
-			System.out.println(search(list, a));
-		}
-		System.out.println(search(list, 15));
+		int[] a = { 2, 3, 1, 2, 2, 2, 2, 2 , 2 , 2 };
+
+		System.out.println(search(a, 2));
+		System.out.println(search(a, 3));
+		System.out.println(search(a, 4));
+		System.out.println(search(a, 1));
+		System.out.println(search(a, 8));
 	}
 
 }
